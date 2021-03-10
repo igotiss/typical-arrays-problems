57d97867f448652af1b241a585dd930ca4424a6a
igotis <igotis@ukr.net>
2021-03-10 21:44:48 +0200

    feat: add function min, max. avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,64 @@
 
 exports.min = function min (array) {
-  return 0;
+ 
+  if (Array.isArray(array) && array.length) {
+    // array exists and is not empty
+    var min = array[0];
+
+  for (i=0; i<array.length; i++) {
+
+    if (typeof(array[i])!=="number") return 0;
+
+    if (array[i]<min) min = array[i];
+
+  }
+  return min;
+  } else {
+    return 0;
+  }
+  
+  
 }
 
+
 exports.max = function max (array) {
-  return 0;
+  
+  if (Array.isArray(array) && array.length) {
+    let max = array[0];
+  
+  for (i=0; i<array.length; i++) {
+    if (typeof(array[i])!=="number") return 0;
+
+    if (array[i]>max) max = array[i];
+
+  }
+  return max;
+  } else {
+    return 0;
+  }  
+  
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  
+  if (Array.isArray(array) && array.length) {
+    let l = (array.length);
+  
+  let summ = 0;
+  
+   
+
+  for (i=0; i<array.length; i++) {
+    if (typeof(array[i])!=="number") return 0;
+    summ += array[i];
+  }
+
+  let avg = summ/l;
+
+  return avg;
+  } else {
+    return 0;
+  }
+  
+  
 }
